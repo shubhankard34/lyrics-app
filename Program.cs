36077b086c4c4a9120762794a6d93497426c58af
Shubhankar Dandekar <shubhankard34@gmail.com>
2020-02-04 12:48:11 -0500

    Make port forwarding work

--- Program.cs
+++ Program.cs
@@ -19,6 +19,6 @@ namespace lyrics_app
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://0.0.0.0:5000");;
     }
 }
